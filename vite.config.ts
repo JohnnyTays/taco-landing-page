f127a0d64212e83d7b4d6bfd936ecb54f8b7ed7a
JohnnyTays <johnnytays@gmail.com>
2026-03-11 09:47:39 -0700

    V9: Critical Rendering Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/taco-landing-page/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
